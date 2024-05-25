3b7f3b988cb1df034139c908274b4615cd2fec75
Паластрова Станислава <palastrovastasa@gmail.com>
2024-05-25 20:59:23 +0500

    7.10

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -1,7 +1,6 @@
-import AbstractView from '../framework/view/abstract-view.js';
-import { createTripInfoTemplate } from '../templates/trip-info-template.js';
-
-export default class TripInfoView extends AbstractView{
+import AbstractView from '../framework/view/abstract-view';
+import { createTripInfoTemplate } from '../templates/trip-info-template';
+export default class TripInfoView extends AbstractView {
   get template() {
     return createTripInfoTemplate();
   }
